d91949987ce799e54655fb672fe06dde06f26550
aswnsyrf <aswnsyrf@gmail.com>
2023-09-22 10:02:36 +0800

    up

---
+++ resources/views/universitas/mahasiswa.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>haikalidiot</title>
+</head>
+<body>
+    <H1>Haikal idiot</H1>
+   <ul>
+    @foreach ($mahasiswa as $isi )
+    <li>{{$isi}}</li>
+
+    @endforeach
+   </ul>
+
+    <script src="js/peringatan.js"></script>
+
+</body>
+</html>
+
