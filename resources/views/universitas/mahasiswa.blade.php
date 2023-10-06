7cfffeadbbd94c1df978dc8478156f2a0abbbdcf
Aswnsyrf <aswnsyrf@gmail.com>
2023-10-06 08:47:51 +0800

    Uji coba

--- resources/views/universitas/mahasiswa.blade.php
+++ resources/views/universitas/mahasiswa.blade.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>haikalidiot</title>
-</head>
-<body>
-    <H1>Haikal idiot</H1>
-   <ul>
-    @foreach ($mahasiswa as $isi )
-    <li>{{$isi}}</li>
+<h2>Data mahasiswa Fakultas {{ $fk }}</h2>
 
+<ul>
+    @foreach ($mhs as $row)
+    <li> <?php echo $row ?> </li>
     @endforeach
-   </ul>
-
-    <script src="js/peringatan.js"></script>
-
-</body>
-</html>
-
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/mahasiswa', function () {
-    $array_nama = ["Haikal","dur","Madi"];
-    return view('universitas.mahasiswa')-> with('mahasiswa',$array_nama);
+Route::get('/mahasiswa/{fakultas}', function ($fakultas) {
+
+    $array = ['Risa', 'Ari'];
+
+    return view('universitas.mahasiswas')
+            ->with([
+                'mhs' => $array,
+                'fk' => $fakultas
+            ]);
+
 });
+
+Route::get('/admin', function () {
+    return view('dashboard');
+});
+
+
+//ujicoba update
